456a2b8ecf255e4a05db79f8dd0de27b1b9f71eb
Zouhair96 <54177116+Zouhair96@users.noreply.github.com>
2026-06-14 20:08:26 +0200

    Add Service class with run and process methods

---
+++ src/main/java/com/opspilot/Service.java
@@ -0,0 +1,7 @@
+// src/main/java/com/opspilot/Service.java
+package com.opspilot;
+public class Service {
+    public void run() {
+        System.out.println("running");
+    }
+    public xyz process() { return null; }   
